3d514006fc31e7c4487e2f0693c7b8f7cf7a2c0a
felipenacif <f.nacif@hotmail.com>
2020-11-18 22:00:36 -0300

    Excluindo o virtualenv

--- home/views.py
+++ home/views.py
@@ -1,4 +1,13 @@
+from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 def home(request):
-    return HttpResponse("Hello, Django!")
+    return render(
+        request,
+        'home/home_page.html',
+        {
+            'name': 'Felipe',
+            'date': datetime.now()
+        }
+    )
